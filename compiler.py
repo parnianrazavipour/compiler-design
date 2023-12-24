1fae7100b80a285cb13258269f7cfeaf3ce8d577
mobinasalimi <mobina.salimipanah@gmail.com>
2023-12-24 14:00:04 +0330

    error handling debug

--- compiler.py
+++ compiler.py
@@ -699,9 +699,13 @@ def parse(token_lists, parsing_table, first_sets, follow_sets):
         elif (top not in parsing_table) or (token not in parsing_table[top]) :
             errors.append(f"#{line_num} : syntax error, illegal {token}")
             index += 1 
+            stack.insert(0,(top , current_node))
         elif  top != token:
             errors.append(f"#{line_num} : syntax error, illegal {token}")
             index += 1 
+            stack.insert(0,(top , current_node))
+
+        
 
     # if not errors and stack:
     Node('$', parent=root)
